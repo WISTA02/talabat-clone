0ade1c113a248f355b28bd48a9e664e5f2e85d6e
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-07-25 09:34:01 +0300

    new relation

--- src/auth/models/resturant.js
+++ src/auth/models/resturant.js
@@ -1,22 +1,14 @@
+"use strict";
 
+const restModel = (sequelize, DataTypes) =>
+  sequelize.define(
+    "resturants",
+    {
+      name: { type: DataTypes.STRING, required: true },
 
-'use strict';
-
-const restModel = (sequelize, DataTypes) => 
-sequelize.define('resturants', {
-  name: {
-     type: DataTypes.STRING, 
-     required: true },
-
- 
-  restaurant_area:{
-    type: DataTypes.STRING, 
-    required: true 
-  },
-
-order_path:{type: DataTypes.INTEGER,}
-},
-{timestamps:false});
-
+      order_path: { type: DataTypes.INTEGER },
+    },
+    { timestamps: false }
+  );
 
 module.exports = restModel;
